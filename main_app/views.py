9ef547944f7075979b455f54e676d4c5670b066f
lejt <92820402+lejt@users.noreply.github.com>
2022-01-11 19:48:46 -0800

    complete part 3 basic

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from .models import finches as finches
+# from .models import finches as finches
+from main_app.models import Finch
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>hi</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches':finches})
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
+
+# class based views below:
+class FinchCreate(CreateView):
+    model = Finch
+    fields = ['name', 'family', 'description', 'fav_food', 'weight']
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['name', 'description', 'fav_food']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
+
